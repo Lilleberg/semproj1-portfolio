e1b61e0090735d07921b004fb7cebbc5c3514af6
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-09-30 20:04:55 +0200

    fixing nav

--- js/global.js
+++ js/global.js
@@ -1,11 +1,29 @@
 const buttonHambMenu = document.querySelector("#hamburger-menu");
 const nav = document.querySelector(".navbar");
+const blurElements = document.querySelectorAll("main, footer");
 
 buttonHambMenu.onclick = function () {
-  console.log("click");
-  if (nav.style.display === "none") {
-    nav.style.display = "block";
+  if (nav.style.display === "block") {
+    styleElements(nav, "none", 0, "<i class='fas fa-bars'></i>");
   } else {
-    nav.style.display = "none";
+    styleElements(nav, "block", 3, "<i class='fas fa-times'></i>");
+  }
+}
+
+function backgroundBlur(blur) {
+  blurElements.forEach(function (elem) {
+    elem.style.filter = `blur(${blur}px)`;
+  });
+}
+
+function styleElements(element, display, blur, icon) {
+  element.style.display = display;
+  buttonHambMenu.innerHTML = icon;
+  backgroundBlur(blur);
+}
+
+document.querySelector("main").onclick = function () {
+  if (nav.style.display === "block") {
+    styleElements(nav, "none", 0, "<i class='fas fa-bars'></i>");
   }
 }
